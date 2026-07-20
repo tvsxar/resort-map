76715b913f2d8af104dab65c3d2446bf8b4225f1
tvsxar <tarasofficial82@gmail.com>
2026-07-20 23:53:40 +0300

    feat: add resort map API

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import express from "express";
+import cors from "cors";
+import fs from "node:fs";
 
 const app = express();
 const PORT = 1999;
 
+const mapText = fs.readFileSync("./map.ascii", "utf-8");
+const mapRows = mapText.trim().split("\n");
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ status: "ok" });
 });
+
+app.get("/map", (req, res) => {
+    res.json({ map: mapRows });
+})
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
